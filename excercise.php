acb33b05c110e2b918bbaea01c9aef39cb18ffb0
kyrenieee <kcestocapio05@gmail.com>
2025-03-06 16:29:47 +0800

    Exercise 2: Conditional Statements

---
+++ excercise.php
@@ -0,0 +1,25 @@
+<?php
+    $a = 15;
+    $b = 7;
+    echo (" Sum: " . $a + $b . "\n");
+    echo ("Difference: " .  $a - $b . "\n");
+    echo ("Product: " . $a * $b . "\n");
+    echo ("Quotient: " . $a / $b . "\n");
+
+    $number =10;
+    
+    if ($number / 2) {
+        echo "number is even" . "\n";
+    } else {
+        echo "number is odd" . "\n";
+    }
+    
+    if ($number < 0) {
+        echo "number is negative" . "\n";
+    } elseif ($number > 1) {
+        echo "number is positive" . "\n";
+    } else {
+        echo "number is zero" . "\n";
+    }
+?>
+
