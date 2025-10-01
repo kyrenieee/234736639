6a833d3c9afe499cc9bf24897f475a310d7c2ee8
kyrenieee <kcestocapio05@gmail.com>
2025-10-01 10:52:38 +0800

    index and intro php

---
+++ CIT17/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Lab 02</h1>
+    <ul>
+        <li><a href="intro.php">Introduce Yourself</a></li>
+        <li><a href="math.php">Simple Math</a></li>
+        <li><a href="rect.php">Area & Parameter of a Rectangle</a></li>
+        <li><a href="temp.php">Temperature Converter</a></li>
+        <li><a href="var.php">Swapping Variables</a></li>
+        <li><a href="calc.php">Salary Calculator</a></li>
+        <li><a href="bmi.php">BMI Calculator</a></li>
+        <li><a href="string.php">String Manipulation</a></li>
+        <li><a href="bank.php">Bank Account Simulation</a></li>
+        <li><a href="grading.php">Simple Grading System</a></li>
+        <li><a href="currency.php">Currency Converter</a></li>
+        <li><a href="travel.php">Travel Cost Estimator</a></li>
+    </ul>
+    
+</body>
+</html>
---
+++ CIT17/intro.php
@@ -0,0 +1,7 @@
+<?php
+    $name = "Kyrene Camilla S. Estocapio";
+    $age = 20;
+    $color = "Purple";
+
+    echo  "<h2>Hi, I'm $name, I am $age years old, and my favorite color is $color.</h2>";
+?>
