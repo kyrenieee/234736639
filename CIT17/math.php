11202c48abf31593d82ac4d300ccbeab41e55d33
kyrenieee <kcestocapio05@gmail.com>
2025-10-01 11:19:09 +0800

    math php

---
+++ CIT17/math.php
@@ -0,0 +1,16 @@
+<?php
+    $a = 20;
+    $b = 2;
+    $sum = $a + $b;
+    $difference = $a - $b;
+    $product = $a * $b;
+    $quotient = $a / $b;
+    
+    echo "<br>Variable A = $a</br>" ;
+    echo "<br>Variable B = $b</br>"; 
+
+    echo "<br>Sum: A + B = $sum</br>";
+    echo "<br>Difference: A - B = $difference</br>";
+    echo "<br>Product: A * B = $product</br>";
+    echo "<br>Quotient: A / B = $quotient</br>";
+?>
